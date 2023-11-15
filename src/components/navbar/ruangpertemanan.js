67f7a2883b4082235363e0129cb7cf4e93ba398f
revofarissaifuddin <revosaifuddin@gmail.com>
2023-11-15 10:12:33 +0700

    rev nav

--- src/components/navbar/ruangpertemanan.js
+++ src/components/navbar/ruangpertemanan.js
@@ -90,7 +90,7 @@ const NavRuangPertemanan = () => {
         </ul>
       </nav>
       {/* Social icons */}
-      <div className="hidden lg:flex fixed flex-col top-[30%] left-0">
+      <div className="hidden lg:flex fixed flex-col top-[30%] left-0 z-50">
         <ul>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-red-700 rounded-md">
             <a
